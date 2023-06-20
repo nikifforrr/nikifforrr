e2f7fd5ee0afdea400f6f2e207a12fb9b28aba1e
abdelrahman <https://github.com/abdo6999>
2023-06-20 15:58:30 +0300

    upload

--- dashboard.js
+++ dashboard.js
@@ -14,3 +14,126 @@ input.addEventListener("input", (e)=>{
         }
     }
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function fillOrderTemplate(orders) {
+  const orderTemplates = [];
+
+  orders.forEach(order => {
+    const addressesContainer = document.createElement('div');
+    addressesContainer.classList.add('addresses');
+
+    const distributionsContainer = document.createElement('div');
+    distributionsContainer.classList.add('distributions');
+
+    const transferDelaysContainer = document.createElement('div');
+    transferDelaysContainer.classList.add('transfer-delays');
+
+    order.wallet_percentage_address.forEach(item => {
+      const addressElement = document.createElement('div');
+      addressElement.textContent = item.address;
+      addressesContainer.appendChild(addressElement);
+
+      const distributionElement = document.createElement('div');
+      distributionElement.textContent = `${item.percentage}%`;
+      distributionsContainer.appendChild(distributionElement);
+
+      const transferDelayElement = document.createElement('div');
+      transferDelayElement.textContent = item.delay;
+      transferDelaysContainer.appendChild(transferDelayElement);
+    });
+
+    const orderItemTemplate = `
+        <div id="order_id">${order._id}</div>
+        <div id="amount">${order.amount}</div>
+        <div id="currency">${order.currency}</div>
+        ${addressesContainer.outerHTML}
+        ${distributionsContainer.outerHTML}
+        ${transferDelaysContainer.outerHTML}
+        <div id="stage">${order.stage}</div>
+        <div>
+          <button id="Advance">Advance</button>
+        </div>
+    `;
+
+    orderTemplates.push(orderItemTemplate);
+  });
+
+  return orderTemplates;
+}
+
+// Fetch orders from the API
+fetch('https://cryptomix.onrender.com/api/orders')
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error('Error fetching orders');
+    }
+  })
+  .then(orders => {
+    const ordersDiv = document.querySelector('.orders-div');
+    
+    // Generate order templates
+    const orderTemplates = fillOrderTemplate(orders);
+    
+    // Append order templates to the orders div
+    orderTemplates.forEach(orderTemplate => {
+      const orderElement = document.createElement('div');
+      orderElement.classList.add("order")
+      orderElement.innerHTML = orderTemplate;
+      ordersDiv.appendChild(orderElement);
+
+      const advanceButton = orderElement.querySelector('#Advance');
+      const orderIdElement = orderElement.querySelector('#order_id');
+      const stageElement = orderElement.querySelector('#stage');
+
+      advanceButton.addEventListener('click', () => {
+        fetch(`https://cryptomix.onrender.com/api/orders/admin-approve/${orderIdElement.textContent}`)
+        .then(response => {
+          if (response.ok) {
+            stageElement.innerText = parseInt(stageElement.textContent) + 1
+            return response.json();
+          } else {
+            response.json().then((response)=>{
+              console.error(response)
+            })
+          }
+        })
+      });
+    });
+  })
+  .catch(error => {
+    console.error(error);
+  });
