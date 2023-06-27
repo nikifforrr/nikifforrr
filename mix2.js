66be4461de48746ba6e79afa34d14f6b64692d4f
abdelrahman <https://github.com/abdo6999>
2023-06-27 15:02:42 +0300

    upload

--- mix2.js
+++ mix2.js
@@ -51,8 +51,6 @@ if(orderId){
     .then(response => {
       if (response.ok) {
         return response.json();
-      }else if(response.status(404)){
-        localStorage.removeItem("order_id")
       }else {
         throw new Error('Error fetching order');
       }
